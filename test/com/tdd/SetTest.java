30dafec4afe50064ef085925bb3bf76f2e89bddc
Srikanth Rentala <srikanth.rentala@realpage.com>
2018-09-30 15:41:22 -0700

    Phase III - extending the contains usage to test methods with one/two elements in the Set...with minimal implementation. - fails - Changing the test method names as well

--- test/com/tdd/SetTest.java
+++ test/com/tdd/SetTest.java
@@ -2,6 +2,7 @@ package com.tdd;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -17,17 +18,21 @@ public class SetTest {
 	}
 	
 	@Test
-	public void testSet_isEmpty_AddOneElement() {
+	public void testSet_WithOneElementAddedtoSet() {
 		Set set = new Set();
 		set.add(1);
 		assertEquals(false, set.isEmpty());
+		assertTrue(set.contains(1));
 	}
 	
 	@Test
-	public void testSet_isEmpty_AddTwoElements() {
+	public void testSet_WithTwoElementsAddedtoSet() {
 		Set set = new Set();
 		set.add(1);
 		set.add(2);
 		assertFalse(set.isEmpty());
+		assertTrue(set.contains(1));
+		assertTrue(set.contains(2));
+		assertFalse(set.contains(3));
 	}
 }
